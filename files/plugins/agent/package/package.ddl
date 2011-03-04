af262b14b814797920aa54fb43b00d7df4a3fa9e
Adam Gandelman <adam@janrain.com>
2011-03-04 19:21:43 +0000

    Import latest package plugins, fixes apt-get update issue.

--- files/plugins/agent/package/package.ddl
+++ files/plugins/agent/package/package.ddl
@@ -2,7 +2,7 @@ metadata    :name        => "SimpleRPC Agent For Package Management",
             :description => "Agent To Manage Packages", 
             :author      => "R.I.Pienaar",
             :license     => "GPLv2",
-            :version     => "1.2",
+            :version     => "1.3",
             :url         => "http://mcollective-plugins.googlecode.com/",
             :timeout     => 180
 
@@ -51,4 +51,17 @@ action "yum_clean", :description => "Clean the YUM cache" do
            :description => "Output from YUM",
            :display_as  => "Output"
 
+    output :exitcode,
+           :description => "The exitcode from the yum command",
+           :display_as => "Exit Code"
+end
+
+action "apt_update", :description => "Updated the apt cache" do
+    output :output,
+           :description => "Output from apt-get",
+           :display_as  => "Output"
+
+    output :exitcode,
+           :description => "The exitcode from the apt-get command",
+           :display_as => "Exit Code"
 end
--- files/plugins/agent/package/puppet-package.rb
+++ files/plugins/agent/package/puppet-package.rb
@@ -10,7 +10,7 @@ module MCollective
                         :description => "Agent To Manage Packages",
                         :author      => "R.I.Pienaar",
                         :license     => "GPLv2",
-                        :version     => "1.2",
+                        :version     => "1.3",
                         :url         => "http://mcollective-plugins.googlecode.com/",
                         :timeout     => 180
 
@@ -22,11 +22,19 @@ module MCollective
             end
 
             action "yum_clean" do
-                if File.exist?("/usr/bin/yum")
-                    reply[:output] = %x[/usr/bin/yum clean all]
-                else
-                    reply.fail "Cannot find yum at /usr/bin/yum"
-                end
+                reply.fail! "Cannot find yum at /usr/bin/yum" unless File.exist?("/usr/bin/yum")
+                reply[:output] = %x[/usr/bin/yum clean all]
+                reply[:exitcode] = $?.exitstatus
+
+                reply.fail! "Yum clean failed, exit code was #{reply[:exitcode]}" unless reply[:exitcode] == 0
+            end
+
+            action "apt_update" do
+                reply.fail! "Cannot find apt-get at /usr/bin/apt-get" unless File.exist?("/usr/bin/apt-get")
+                reply[:output] = %x[/usr/bin/apt-get update]
+                reply[:exitcode] = $?.exitstatus
+
+                reply.fail! "apt-get update failed, exit code was #{reply[:exitcode]}" unless reply[:exitcode] == 0
             end
 
             private
@@ -34,11 +42,11 @@ module MCollective
                 begin
                     require 'puppet'
 
-                    if Puppet.version =~ /0.24/
-                        Puppet::Type.type(:package).clear
-                        pkg = Puppet::Type.type(:package).create(:name => package).provider
+                    if ::Puppet.version =~ /0.24/
+                        ::Puppet::Type.type(:package).clear
+                        pkg = ::Puppet::Type.type(:package).create(:name => package).provider
                     else
-                        pkg = Puppet::Type.type(:package).new(:name => package).provider
+                        pkg = ::Puppet::Type.type(:package).new(:name => package).provider
                     end
 
                     reply[:output] = ""
