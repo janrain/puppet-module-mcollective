e95aca692604d68619f4ceb8416cf284fc14ffa4
James Loope <jloope@janrain.com>
2011-08-02 23:13:57 +0000

    changes for new mcollective

--- manifests/plugin/facter.pp
+++ manifests/plugin/facter.pp
@@ -1,5 +1,6 @@
 
 class mcollective::plugin::facter {
     mcollective::plugin_file { "facts/facter_facts.rb": source => "facts/facter/facter_facts.rb" }
+    mcollective::plugin_file { "facts/facter.rb": source => "facts/facter/facter_facts.rb" }
 }
 
