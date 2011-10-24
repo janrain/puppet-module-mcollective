ba38c16c4ebc122b79557c10aba43912172646e3
jloope <jloope@janrain.com>
2011-10-24 16:23:19 -0700

    fix ruby conflict

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -1,23 +1,24 @@
 
 class mcollective::install::debian {
-    package { "ruby":
-        ensure => installed,
-    }   
-    package { "rubygems":
-        ensure => installed,
-    }
-    package { "stomp": 
-        provider => gem,
-        require => Package["ruby", "rubygems"],
-        ensure => "1.1.9",
-        notify => Service["mcollective"],
-    }
+        if ! defined(Package[ruby]) {
+        package { "ruby":
+                ensure => installed,
+        }
+        package { "rubygems":
+                ensure => installed,
+        }
+        package { "stomp":
+                provider => gem,
+                require => Package["ruby", "rubygems"],
+                ensure => "1.1.9",
+                notify => Service["mcollective"],
+        }
+        }
 
-
-    # package { "mcollective":
-    # NOTE: This package comes from the puppet repository, which is  
-    #       setup via puppetd::client.  This package gets declared 
-    #	    in apps::mcollective (dependent on puppetd::client).
-    #       Any Package["mcollective*"] requirements in this module
-    #	    refers to  those declared in apps::mcollective + client -AG
+        # package { "mcollective":
+        # NOTE: This package comes from the puppet repository, which is
+        #       setup via puppetd::client.  This package gets declared
+        #       in apps::mcollective (dependent on puppetd::client).
+        #       Any Package["mcollective*"] requirements in this module
+        #       refers to  those declared in apps::mcollective + client -AG
 }
