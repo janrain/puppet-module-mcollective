646bc2eb01fedded634b7eb672867c9bc32726ab
jloope <jloope@janrain.com>
2011-10-24 17:03:13 -0700

    put the chain inside the class on the gem

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -3,10 +3,9 @@ class mcollective::install::debian {
 
         package { "stomp":
                 provider => gem,
-                require => Package["ruby", "rubygems"],
                 ensure => "1.1.9",
                 notify => Service["mcollective"],
-        }
+        } -> Class[apps::ruby]
 
         # package { "mcollective":
         # NOTE: This package comes from the puppet repository, which is
@@ -14,4 +13,4 @@ class mcollective::install::debian {
         #       in apps::mcollective (dependent on puppetd::client).
         #       Any Package["mcollective*"] requirements in this module
         #       refers to  those declared in apps::mcollective + client -AG
-} -> Class[apps::ruby]
+}
