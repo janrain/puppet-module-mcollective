706455c9b6eea1bde1553419efeba0b71de45034
John Eikenberry <jae@zhar.net>
2011-10-28 13:44:51 -0700

    change chaining of ruby class to require

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -1,11 +1,11 @@
 
 class mcollective::install::debian {
 
+        $require = Class[apps::ruby]
         package { "stomp":
                 provider => gem,
                 ensure => "1.1.9",
                 notify => Service["mcollective"],
-        } -> Class[apps::ruby]
 
         # package { "mcollective":
         # NOTE: This package comes from the puppet repository, which is
